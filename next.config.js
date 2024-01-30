c64d12199f4e66c60cce561917e09f6c21cb1a94
Siddharth9890 <siddharth.a9890@gmail.com>
2024-01-30 17:00:57 +0530

    chore: testing vercel prod #8

--- next.config.js
+++ next.config.js
@@ -1,17 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
+  webpack: (config, { dev }) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
-    config.externals.push(
-      "fast-json-stringify",
-      "@whatwg-node/fetch",
-      "long",
-      "uglify-es",
-      "@graphql-mesh/utils",
-      "@graphql-mesh/tools",
-      "@graphql-tools/url-loader"
-    );
+    if (dev !== false)
+      config.externals.push(
+        "fast-json-stringify",
+        "@whatwg-node/fetch",
+        "long",
+        "uglify-es",
+        "@graphql-mesh/utils",
+        "@graphql-mesh/tools",
+        "@graphql-tools/url-loader"
+      );
+
     return config;
   },
 };
