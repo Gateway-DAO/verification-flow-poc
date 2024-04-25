d824d9943e0ee8bf58b5e83e28f13ebf8aa4d326
Siddharth9890 <siddharth.a9890@gmail.com>
2024-04-25 20:01:20 +0530

    chore: debug logs enabled

--- src/app/api/issue/issue-pda/route.ts
+++ src/app/api/issue/issue-pda/route.ts
@@ -3,7 +3,6 @@ import { Gateway, UserIdentifierType } from "@gateway-dao/sdk";
 export async function POST(req: Request) {
   try {
     const body = await req.json();
-
     const gateway = new Gateway({
       apiKey: process.env.API_KEY as string,
       token: process.env.BEARER as string,
@@ -12,10 +11,6 @@ export async function POST(req: Request) {
     const data = await gateway.pda.createPDA({
       title: body.title,
       description: body.description,
-      organization: {
-        type: "GATEWAY_ID",
-        value: process.env.ORG_GATEWAY_ID as string,
-      },
       owner: { type: UserIdentifierType.EVM, value: body.address },
       dataModelId: process.env.DATA_MODEL_ID as string,
       claim: body.claim,
@@ -26,6 +21,7 @@ export async function POST(req: Request) {
       pda: data?.createPDA,
     });
   } catch (error) {
+    console.log(error);
     console.log(JSON.stringify(error, null, 4));
 
     return Response.json({ error }, { status: 500 });
